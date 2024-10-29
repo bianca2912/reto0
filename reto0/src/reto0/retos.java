ee8e0fda3be03501a2d659ae87fa45a629ffb461
Roberto-byte2 <robertorroba@gmail.com>
2024-10-29 09:08:19 +0100

    hola mundo tengo hambre y sueño

--- reto0/src/reto0/retos.java
+++ reto0/src/reto0/retos.java
@@ -4,7 +4,7 @@ public class retos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo, tengo sueÃ±o.");
+		System.out.println("Hola mundo, tengo sueño y hambre.");
 
 	}
 
